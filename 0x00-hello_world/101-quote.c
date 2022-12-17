f37992879f128bf029ae66c5b87a54bb4e4eb758
Joachim <joachim.wat@gmail.com>
2022-12-17 22:19:31 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - A C program that prints a line to the standard error
- * Return 1 (Success)
+ * main- Entry point
+ * @void: parameter
+ * Return: 1 (success)
  */
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+
+	return (1);
 }
