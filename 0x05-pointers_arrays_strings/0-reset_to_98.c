cde230dd48954f4b55c3f52002efb9d44aa0f731
Joachim <joachim.wat@gmail.com>
2022-12-20 05:18:57 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
- * reset_to_98 - update value of n to 98
+ * reset_to_98: update value of n to 98
  * @n: param
  *
  */
 
-void reset_to_98 (int *n) 
+void reset_to_98(int *n) 
 {
 	*n = 98;
 }
